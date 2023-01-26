7a17d3f42d1749b449863f00495e74c6c954c8fe
Shawn Johnson <johnson.sg@gmail.com>
2023-01-25 19:32:20 -0500

    added another reducer

--- src/context/todos.context.js
+++ src/context/todos.context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext } from 'react'
+import { useLocalStorageReducer } from '../hooks/useLocalStorageReducer'
 import todoReducer from '../reducers/todo.reducer'
 
 const defaultTodos = []
@@ -7,7 +8,11 @@ export const TodosContext = createContext()
 export const DispatchContext = createContext()
 
 export function TodosProvider(props) {
-  const [todos, dispatch] = useReducer(todoReducer, defaultTodos)
+  const [todos, dispatch] = useLocalStorageReducer(
+    'todos', // first argument is the "key" we are looking for in useLocalStorageReducer
+    defaultTodos, // second argument is the "defaultVal"
+    todoReducer // third is the "reducer"
+    )
   return (
     <TodosContext.Provider value={todos}>
       <DispatchContext.Provider value={dispatch}>
